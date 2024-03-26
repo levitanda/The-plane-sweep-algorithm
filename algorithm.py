419a28ee2618cf30684db42e3e8842c064b3d49b
levitanda <dalevitan17@gmail.com>
2024-03-26 20:33:06 +0200

    counts intersects, but still need to add swap for intersects

--- algorithm.py
+++ algorithm.py
@@ -1,23 +1,64 @@
-import modules
+#import modules
+import mod
 
 def parsing_single_testcase(file, segments):
     num_segments = int(file.readline())
     for id in range(num_segments):
-        x1, y1, x2, y2 = map(int, file.readline().split())
-        p = modules.Point(x1, y1)
-        q = modules.Point(x2, y2)
-        segment = modules.Segment(id,p,q)
+        x1, y1, x2, y2 = map(float, file.readline().split())
+        p = mod.Point(x1, y1)
+        q = mod.Point(x2, y2)
+        segment = mod.Segment(id,p,q)
         segments.append(segment)
 
-def event_priority_queue(segments, event_priority_queue):
+def event_queue_initialization(segments, event_priority_queue):
     for seg in segments:
-        
+        #0 - beggining of segment, 1 - ending of segment
+        event_priority_queue.insert((seg,0), seg.q.x)
+        event_priority_queue.insert((seg,1), seg.p.x)
+
+def sweep(events):
+    status = None
+    while not events.empty():
+        ev = events.delete()
+        if(ev[1] == False):
+            #add_if_intersection
+            mod.deleteNode(status, ev[0].id)
+
+
 
 def main():
+    filename = "input.txt"
+    print("starting parsing...")
     with open(filename, "r") as file:
         num_test_cases = int(file.readline())
-        for _ in range(num_test_cases):
+        print("num of test cases ", num_test_cases)
+        for i in range(num_test_cases):
             segments = []
             parsing_single_testcase(file, segments)
-            event_priority_queue = modules.CG24PriorityQueue()
-            event_gueue_initialization(segments, event_priority_queue)
+            event_priority_queue = mod.CG24PriorityQueue()
+            event_queue_initialization(segments, event_priority_queue)
+            print("the tast case # ",i)
+            counter = 0
+            sweep_line_status = mod.SegmentBST()
+            while not event_priority_queue.empty():
+                s = event_priority_queue.pop()
+                if s[1]==0: #segment begin
+                    print(s[0].q.x, s[0].id)
+                    sweep_line_status.insert(s[0], s[0].q.x, event_priority_queue)
+                elif s[1]==1: #segment finish
+                    print(s[0].p.x, s[0].id)
+                    sweep_line_status.remove(s[0], s[0].p.x, event_priority_queue)
+                elif s[1]==2: #intersection
+                    print(s[0].x, s[0].y)
+                    counter += 1
+                    seg1 = s[2]
+                    seg2 = s[3]
+                    #sweep_line_status.remove(s[0], s[0].x, event_priority_queue)
+                    
+            print("intersects num: ", counter)
+                    
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ mod.py
@@ -0,0 +1,307 @@
+class Point:
+    x : float # float
+    y : float # float
+    
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    # def
+# class
+
+class Segment:
+    id : int # to overcome numerical error when we find a point on an ...
+    #    # already-known segment we identify segments with unique ID.
+    #    # binary search with numerical errors is guaranteed to find an ...
+    #    # index whose distance from the correct one is O(1) (here it is 2).
+    #
+    p : Point # Point, after input we compare and swap to guarantee that p.x <= q.x
+    q : Point # Point
+    
+    def __init__(self,id,p,q):
+        if p.x > q.x:
+            p,q = q,p
+        self.p = p
+        self.q = q
+        self.id = id
+    # def
+    
+    # line: y = ax + b. it is guaranteed that the line is not vertical (a is finite)
+    def a(self): # () -> double
+        return ((self.p.y - self.q.y) / (self.p.x - self.q.x))
+    # def
+    
+    def b(self): # () -> double
+        return (self.p.y - (self.a() * self.p.x))
+    # def
+    
+    # the y-coordinate of the point on the segment whose x-coordinate ..
+    #   is given. Segment boundaries are NOT enforced here.
+    def calc(self, x):
+        return (self.a() * x + self.b())
+    # def
+# class
+
+def is_left_turn(a, b, c): # (Point,Point,Point) -> bool
+    x1 = a.x
+    x2 = b.x
+    x3 = c.x
+    y1 = a.y
+    y2 = b.y
+    y3 = c.y
+    return ((x1 * (y2 - y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2))) > 0
+# def
+
+def intersection(s1, s2): # (segment,segment) -> Point | None
+    if s1 != None and s2 != None:
+        if ((is_left_turn(s1.p, s1.q, s2.p) != is_left_turn(s1.p, s1.q, s2.q)) and
+            (is_left_turn(s2.p, s2.q, s1.p) != is_left_turn(s2.p, s2.q, s1.q))):
+            
+            a1 = s1.a()
+            a2 = s2.a()
+
+            b1 = s1.b()
+            b2 = s2.b()
+
+            # commutation consistency: sort by a (then by b)
+            if a1 > a2 or (a1 == a2 and b1 > b2):
+                a1,a2 = a2,a1
+                b1,b2 = b2,b1
+            # if
+
+            #
+            # a1 x + b1 = y
+            # a2 x + b2 = y
+            # (a1 - a2)x + (b1-b2) = 0
+            # x = (b2-b1)/(a1-a2)
+            #
+
+            x = (b2 - b1) / (a1 - a2)
+            y = s1.calc(x)
+
+            return Point(x, y)
+    else:
+        return None
+    #else
+#def
+
+def intersects(s1, s2): # (Segment,Segment) -> bool
+    return not(intersection(s1, s2) is None)
+#def
+
+
+class CG24PriorityQueue:
+    max1 : bool# bool
+    max2 : bool # bool
+    max3 : bool # bool
+    t    : int # int
+    arr  : any # any[]
+
+    class cEntry:
+        p : float
+        #p2  # double
+        #p3  # double
+        pzm : int
+        data : any
+        def __init__(self):
+            pass
+
+        def print(self):
+            print(self.p)
+    # class
+    
+    def __init__(self, priorityMax=True, tiebreakerMax=True, tiebreaker2Max=True):
+        self.max1 = priorityMax
+        self.max2 = tiebreakerMax
+        self.max3 = tiebreaker2Max
+        self.t    = int(0)
+        self.arr  = list()
+    # def
+    
+    def compare(self, l, r): # (p1,p2) -> bool
+        if l.p != r.p:
+            return (l.p > r.p) if self.max1 else (l.p < r.p)
+        if l.p2 != r.p2:
+            return (l.p2 > r.p2) if self.max2 else (l.p2 < r.p2)
+        if l.p3 != r.p3:
+            return (l.p3 > r.p3) if self.max3 else (l.p3 < r.p3)
+        return l.pzm < r.pzm
+    # def
+    
+    def insert(self, data, p, tiebreaker=0, tiebreaker2=0): # (any, double[, double[, double]]) -> void
+        entry      = CG24PriorityQueue.cEntry()
+        entry.p    = float(p)
+        entry.p2   = float(tiebreaker)
+        entry.p3   = float(tiebreaker2)
+        entry.pzm  = self.t
+        entry.data = data
+        
+        self.t = self.t + int(1)
+        self.arr.append(entry)
+        # heapify up
+        i = int(len(self.arr)) - int(1)
+        parent = int(i / 2)
+        while i != parent and self.compare(self.arr[i], self.arr[parent]):
+            self.arr[i], self.arr[parent] = self.arr[parent], self.arr[i]
+            i = parent
+            parent = int(i / 2)
+        # for i in self.arr:
+        #     i.print()
+        # print("next elem")
+    # def
+    
+    def empty(self): # () -> bool
+        return 0 == len(self.arr)
+    # def
+    
+    def pop(self): # () -> any
+        if 0 == len(self.arr):
+            return self.arr[0] # raise exception
+        
+        res = self.arr[0].data
+        
+        if len(self.arr) > 1:
+            n = len(self.arr)
+            self.arr[0], self.arr[n - 1] = self.arr[n - 1], self.arr[0]
+            n = n - 1
+            i  = 0
+            while i < n:
+                best = i
+                j1 = int(2 * i + 1)
+                j2 = int(2 * i + 2)
+                if j1 < n and self.compare(self.arr[j1], self.arr[best]):
+                    best = j1
+                #if
+                if j2 < n and self.compare(self.arr[j2], self.arr[best]):
+                    best = j2
+                #if
+                if best == i:
+                    break
+                #if
+                self.arr[i], self.arr[best] = self.arr[best], self.arr[i]
+                i = best
+            # while
+        # if
+        self.arr.pop()
+        return res
+    # def
+# class
+    
+
+class TreeNode:
+    def __init__(self, segment):
+        self.segment = segment
+        self.left = None
+        self.right = None
+
+class SegmentBST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, segment, x, event_queue):
+        self.root = self._insert_recursive(self.root, segment, event_queue, x)
+
+    def _insert_recursive(self, root, segment, event_queue, x, last_smallest = None, last_biggest = None):
+        if root is None:
+            if last_biggest != None:
+                k = intersection(segment,last_biggest)
+                if k != None:
+                    event_queue.insert((k,2,segment, last_biggest), k.x)
+            if last_smallest != None:
+                l = intersection(segment, last_smallest)
+                if l != None:
+                    event_queue.insert((l,2,segment,last_smallest), l.x)
+            return TreeNode(segment)      
+        if (segment.calc(x) < root.segment.calc(x)):
+            root.left = self._insert_recursive(root.left, segment, event_queue, x, last_smallest, root.segment)
+        else:
+            root.right = self._insert_recursive(root.right, segment,event_queue, x, root.segment, last_biggest)
+        return root
+    
+    def empty(self):
+        return self.root == None
+
+    def remove(self, segment, x, event_queue):
+        self.root = self._remove_recursive(self.root, segment, x, event_queue)
+
+    def _remove_recursive(self, root, segment, x, event_queue, parent = None):
+        if root is None:
+            return None
+        if segment.calc(x) < root.segment.calc(x):
+            root.left = self._remove_recursive(root.left, segment, x, event_queue, root)
+        elif segment.calc(x) > root.segment.calc(x):
+            root.right = self._remove_recursive(root.right, segment, x, event_queue, root)
+        elif segment.calc(x) == root.segment.calc(x) and segment.id == root.segment.id:
+            neighbour_small, neighbour_big = self.get_neighbours(root.left, root.right, parent, root, x)
+            if neighbour_big != None and neighbour_small != None:
+                l = intersection(neighbour_big.segment, neighbour_small.segment)
+                if l != None:
+                    event_queue.insert((l,2,neighbour_big.segment, neighbour_small.segment), l.x)
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            min_right = self._find_min(root.right)
+            root.segment = min_right.segment
+            root.right = self._remove_recursive(root.right, min_right.segment, x, event_queue)    
+        return root
+
+    def _find_min(self, root):
+        current = root
+        while current.left:
+            current = current.left
+        return current
+    
+    def get_neighbours(self, left, right, parent, root, x):
+        difs = []
+        if left != None:
+            difs.append((left,abs(root.segment.calc(x) - left.segment.calc(x))))
+        if right != None:
+            difs.append((left,abs(root.segment.calc(x) - right.segment.calc(x))))
+        if parent != None:
+            difs.append((left,abs(root.segment.calc(x) - parent.segment.calc(x))))
+        difs.sort(key=lambda a: a[1], reverse=True)
+        if len(difs) >=2:
+            return difs[0][0], difs[1][0]
+        elif len(difs) >=1: 
+            return difs[0][0], None
+        else:
+            return None, None
+
+
+    # def inorder_traversal(self):
+    #     result = []
+    #     self._inorder_traversal_recursive(self.root, result)
+    #     return result
+
+    # def _inorder_traversal_recursive(self, root, result):
+    #     if root:
+    #         self._inorder_traversal_recursive(root.left, result)
+    #         result.append(root.segment)
+    #         self._inorder_traversal_recursive(root.right, result)
+
+
+
+# # Example usage:
+# if __name__ == "__main__":
+#     # Define your Segment class with attributes id and y
+#     class Segment:
+#         def __init__(self, id, y):
+#             self.id = id
+#             self.y = y
+    
+#     # Create an instance of SegmentBST
+#     bst = SegmentBST()
+    
+#     # Insert segments
+#     segments = [Segment(1, 5), Segment(2, 3), Segment(3, 7)]
+#     for segment in segments:
+#         bst.insert(segment)
+    
+#     # Remove a segment by id
+#     bst.remove(2)
+    
+#     # Print inorder traversal of BST
+#     print("Inorder traversal of BST:")
+#     for segment in bst.inorder_traversal():
+#         print(f"Segment id: {segment.id}, y: {segment.y}")
